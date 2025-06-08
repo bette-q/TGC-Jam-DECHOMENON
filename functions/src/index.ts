8cd1c23545f575ddd79e921729eadeaed590bf2a
Betty Qin <bettyqin233@gmail.com>
2025-06-08 17:50:59 -0500

    fb package

---
+++ functions/src/index.ts
@@ -0,0 +1,8 @@
+﻿import * as functions from "firebase-functions";   // ← missing import
+
+// Optional: define the response type so Unity gets IntelliSense
+/*type HelloResponse = { reply: string };
+*/
+export const helloWorld = functions.https.onCall((_data, _ctx) => {
+    return { reply: "Hello from Firebase Functions 👋" };
+});
